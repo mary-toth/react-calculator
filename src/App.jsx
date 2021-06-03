42cdbad0aa912a4423db369a7a7b35c0c4473190
Mary Toth <maryarylynn@gmail.com>
2021-06-03 13:52:54 -0400

    html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <main>
+      <div className="calculator">
+        <div className="display">0</div>
+        <div className="buttons">
+          <button className="button fn">AC</button>
+          <button className="button fn">&plusmn;</button>
+          <button className="button fn">%</button>
+          <button className="button op">&divide;</button>
+          <button className="button">7</button>
+          <button className="button">8</button>
+          <button className="button">9</button>
+          <button className="button op">&times;</button>
+          <button className="button">4</button>
+          <button className="button">5</button>
+          <button className="button">6</button>
+          <button className="button op">&minus;</button>
+          <button className="button">1</button>
+          <button className="button">2</button>
+          <button className="button">3</button>
+          <button className="button op">+</button>
+          <button className="button x2">0</button>
+          <button className="button">.</button>
+          <button className="button op">=</button>
+        </div>
+      </div>
+    </main>
+  )
 }
